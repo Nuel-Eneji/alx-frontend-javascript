dc26b05b28e13c5ffa16c13ba82e21b647073886
Nuel-Eneji <enejiemmanueladie@gmail.com>
2023-06-30 00:31:28 +0000

    corrections

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,15 +1,30 @@
-import signUpUser from './4-user-promise';
-import uploadPhoto from './5-photo-reject';
+import { signUpUser } from './4-user-promise.js';
+import { uploadPhoto } from './5-photo-reject.js';
 
-export default function handleProfileSignup(firstName, lastName, fileName) {
-  return Promise.allSettled([
-    signUpUser(firstName, lastName),
-    uploadPhoto(fileName),
-  ]).then((values) => {
-    const arr = [];
-    for (const item of values) {
-      arr.push({ status: item.status, value: item.value || item.reason });
-    }
-    return arr;
-  });
+async function handleProfileSignup(firstName, lastName, fileName) {
+  const userPromise = signUpUser(firstName, lastName);
+  const photoPromise = uploadPhoto(fileName);
+
+  let userResult;
+  let photoResult;
+
+  try {
+    userResult = await userPromise;
+  } catch (error) {
+    userResult = error;
+  }
+
+  try {
+    photoResult = await photoPromise;
+  } catch (error) {
+    photoResult = error;
+  }
+
+  return [
+    { status: userPromise.status, value: userResult },
+    { status: photoPromise.status, value: photoResult },
+  ];
 }
+
+export default handleProfileSignup;
+
